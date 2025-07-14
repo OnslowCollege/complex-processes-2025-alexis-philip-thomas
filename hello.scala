673e029fb2d6e65b78d1e9a533af88882279b225
unknown <alexis.clemente@student.onslow.school.nz>
2025-07-15 10:41:48 +1200

    committing from alexis

--- hello.scala
+++ hello.scala
@@ -2,5 +2,6 @@
 def hello(): Unit =
     val x = 1
     val y = 2
-    println(x + y)
+    val z = 3
+    println(x + y + z)
     println("just making this change in order to prove i can commit and push changes")
