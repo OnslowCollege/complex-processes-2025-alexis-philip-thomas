16622a4184f87130049fb2aa7e68f2e122e05ca8
unknown <alexis.clemente@student.onslow.school.nz>
2025-07-18 13:03:57 +1200

    starting development of files and folders

--- hello.scala
+++ hello.scala
@@ -1,7 +1,2 @@
 @main
-def hello(): Unit =
-    val x = 1
-    val y = 2
-    val z = 3
-    println(x + y + z)
-    println("just making this change in order to prove i can commit and push changes")
+// 
