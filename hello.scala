79ae23d33e070cd0ebef639cef060ccca2f32ef0
unknown <alexis.clemente@student.onslow.school.nz>
2025-07-22 10:47:37 +1200

    added classes: files and folders

--- hello.scala
+++ hello.scala
@@ -1,2 +1,14 @@
 @main
-// 
+// Classes - Files & Folders
+class File extends Folder {
+    var name: String
+}
+
+class Folder {
+    var name: String
+    var files: [File] =>> [List]
+}
+
+var folder = Folder(name: "MyFolder")
+var file = File(name: "README.txt")
+
