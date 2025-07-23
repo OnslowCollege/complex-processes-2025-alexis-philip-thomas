20d620a3a93550727a67a311c6115f4fb174dde4
unknown <alexis.clemente@student.onslow.school.nz>
2025-07-23 15:02:28 +1200

    beginning to print out the names of the files and folders

--- hello.scala
+++ hello.scala
@@ -1,14 +1,15 @@
-@main
+@main def welcome() = println("Welcome to File Explorer")
 // Classes - Files & Folders
-class File extends Folder {
-    var name: String
+class File(var FileName: String) {
+    println("README.txt")
 }
 
-class Folder {
-    var name: String
-    var files: [File] =>> [List]
+class Folder(var FolderName: String) {
+    println("MyFolder")
 }
+    //val folder1 = Files("MyFolder")
+    //var name: String
+    //var files: [File] =>> [List]
 
-var folder = Folder(name: "MyFolder")
-var file = File(name: "README.txt")
-
+//var folder = Folder(name: "MyFolder")
+//var file = File(name: "README.txt")
