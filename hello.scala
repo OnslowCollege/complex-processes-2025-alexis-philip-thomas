27ddce9ce5051d9f01be2a9cbf897df45f9b1071
unknown <alexis.clemente@student.onslow.school.nz>
2025-07-30 14:43:11 +1200

    created terminal to take in user input

--- hello.scala
+++ hello.scala
@@ -1,27 +1,19 @@
-package system
 // readLine import
 import scala.io.StdIn.readLine
 
-// Classes - Files & Folders
-object DirectoryInteractive {
-    def main(args: Array[String]): Unit = {
-        val show = new File()
-        show.FolderName()
-        show.FileName()
+// Terminal
+@main def terminal() =
+    println("Your command (Enter end to quit): ")
+    var input: String = ""
+    while (input != "end") {
+        println("")
+        var input = readLine()
+        if (input != "end") {
+            if (input == "dir") {
+                println("MyFolder")
+            }
+        }
+    println("Ending task")
+    System.exit(0)
     }
-}
-
-import system.DirectoryInteractive.main
-
-class Folder {
-    def FolderName(): Unit = {
-        println("MyFolder")
-    }
-}
-
-class File extends Folder {
-    def FileName(): Unit = {
-        println("README.txt")
-    }
-}
-
+    
