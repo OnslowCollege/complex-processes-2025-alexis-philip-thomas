fef3da30b4011eac11720f09fb589aefe0802b28
unknown <alexis.clemente@student.onslow.school.nz>
2025-08-01 12:58:22 +1200

    added traits and classes

--- hello.scala
+++ hello.scala
@@ -1,19 +1,42 @@
 // readLine import
 import scala.io.StdIn.readLine
 
+// Description of Files & Folders
+trait FolderDetails {
+    var name: String
+    var size: Int
+}
+
+trait FileDetails {
+    var name: String
+    var size: Int
+}
+
+// Classes - Files & Folders
+class Folder(var name: String, var size: Int) extends FolderDetails {
+    override def toString = s"$name/   (dir - $size KB)"
+}
+
+abstract class File(var name: String, size: Int) extends FileDetails {
+    override def toString = s"$name    $size KB"
+}
+
+var folder1 = Folder("MyFolder", 1)
+var file1 = File("README.txt", 1)
+
 // Terminal
-@main def terminal() =
-    println("Your command (Enter end to quit): ")
-    var input: String = ""
-    while (input != "end") {
-        println("")
-        var input = readLine()
-        if (input != "end") {
-            if (input == "dir") {
-                println("MyFolder")
+object FileSystem
+    def main(args: Array[String]): Unit = {
+        var folder1 = new Folder("MyFolder", 1)
+        var file1 = new File("README.txt", 1)
+        println("Enter your command (Enter end to quit): ")
+        var input: String = ""
+        while (input != "end") {
+            var input = readLine()
+            if (input == "ls") {
+                println(folder1)
+            }
+        println("Ending task")
+        System.exit(0)
             }
         }
-    println("Ending task")
-    System.exit(0)
-    }
-    
