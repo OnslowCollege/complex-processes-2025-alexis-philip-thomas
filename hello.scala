279a9dc587e1d031c1b5a8bf98b9d06e4e113ff3
unknown <alexis.clemente@student.onslow.school.nz>
2025-08-06 14:56:46 +1200

    created commands ls and command end works correctly

--- hello.scala
+++ hello.scala
@@ -17,26 +17,25 @@ class Folder(var name: String, var size: Int) extends FolderDetails {
     override def toString = s"$name/   (dir - $size KB)"
 }
 
-abstract class File(var name: String, size: Int) extends FileDetails {
+class File(var name: String, var size: Int) extends FileDetails {
     override def toString = s"$name    $size KB"
 }
-
-var folder1 = Folder("MyFolder", 1)
-var file1 = File("README.txt", 1)
 
 // Terminal
 object FileSystem
     def main(args: Array[String]): Unit = {
-        var folder1 = new Folder("MyFolder", 1)
-        var file1 = new File("README.txt", 1)
-        println("Enter your command (Enter end to quit): ")
+        var folder1 = Folder("MyFolder", 1)
+        var file1 = File("README.txt", 1)
         var input: String = ""
         while (input != "end") {
+            println("Enter your command (Enter end to quit): ")
             var input = readLine()
             if (input == "ls") {
                 println(folder1)
             }
-        println("Ending task")
-        System.exit(0)
+            if (input == "end") {
+                println("Ending task")
+                System.exit(0)
+                }
             }
         }
