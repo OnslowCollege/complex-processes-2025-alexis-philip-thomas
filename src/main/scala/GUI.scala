7891f5facd6864508630c6121940e3454bf71621
filip <philip.thomson@student.onslow.school.nz>
2025-08-31 21:53:17 +1200

    begun working on the gui using textField

--- src/main/scala/GUI.scala
+++ src/main/scala/GUI.scala
@@ -1,7 +1,7 @@
 import javafx.application.Application
 import javafx.stage.Stage
 import javafx.scene.Scene
-import javafx.scene.control.{Button, Label}
+import javafx.scene.control.{Button, Label, TextField}
 import javafx.scene.layout.VBox
 
 object Main {
@@ -12,12 +12,32 @@ object Main {
 
 class MyApp extends Application {
   override def start(stage: Stage): Unit = {
-    val label = new Label("Hello from Scala JavaFX")
-    val button = new Button("Click me")
+    val label = new Label("/home")
+    val label2 = new Label("")
+    val textField = new TextField()
+    textField.setPromptText("enter your command")
 
-    button.setOnAction(_ => label.setText("Button clicked"))
+    val button = new Button("confirm")
 
-    val layout = new VBox(10, label, button)
+    button.setOnAction(_ => { 
+      val input = textField.getText.toLowerCase.trim
+      val parts = input.split("\\s+").filter(_.nonEmpty)
+      
+
+      if (parts(0) == "ls") {
+        label.setText("will get back to it")
+      } else  if (parts(0) == "cd") {
+        if (parts(1) == "test") {
+          label2.setText("detected test")
+        } else {
+          label2.setText(s"don't recongnize ${parts(1)}")
+        }
+      } else {
+        label2.setText(s"dont recognize $input")
+      }
+    })
+
+    val layout = new VBox(10, label, label2, textField, button)
     val scene = new Scene(layout, 300, 200)
 
     stage.setTitle("Scala JavaFX App")
