914978b1f098796547d4bc600c8d5259d1cb14a4
ac-gif <alexis.clemente@student.onslow.school.nz>
2025-09-12 12:42:42 +1200

    making code more efficient to work better with philip's gui, added content, made node aware of parent

--- src/main/scala/hello.scala
+++ src/main/scala/hello.scala
@@ -15,34 +15,67 @@ case class File(name: String, val size: Int = 1) extends Node
 // Makes the file (child node) aware of its parent folder
 case class NodeContext(parentName: String, leftnodes: List[Node], rightNodes: List[Node])
 
+// Zipper - a focus on a node with context relating that node to the root folder
 case class Zipper(focus: Node, context: List[NodeContext])
+
+// To print path in terminal
+def printPath(z: Zipper): String =
+    def loop(z: Zipper, accumulated: List[String]): List[String] =
+        z.context match
+            case Nil => 
+                // If child file doesn't have parent folder
+                z.focus.name :: accumulated
+            case NodeContext(parentName, left, right) :: rest =>
+                // If child file does have parent folder - loop over node repeatedly
+                /* Loop on a zipper created with a folder created with this object's parent's name,
+                and children including this node's siblings (left-side siblings joined to this node
+                and right-side siblings). The new zipper's context is the whole tail of the base zipper's
+                context. The accumulation of the loop is the new zipper's folder's name at the head of
+                the current accumulation/path, building the path. */
+                loop(Zipper(Folder(parentName, left ::: (z.focus :: right)), rest), z.focus.name :: accumulated)
+            loop(z, Nil).mkString("/")
 
 // Commands
 // Command - ls
 def ls(z: Zipper): Unit =
     z.focus match
         case Folder(_, children) =>
-        children.foreach {
-            case Folder(name, _) => println(s"/$name")
-            case File(name, size) => println(s"name   ${size}KB")
-        }
+            children.foreach {
+                case Folder(name, _) => println(s"/$name")
+                case File(name, size) => println(s"name   ${size}KB")
+            }
         case _ =>
             None
 
 
-// Terminal
-object FileSystem
-    def main(args: Array[String]): Unit = {
-        var input: String = ""
-        while (input != "end") {
-            println("Enter your command (Enter end to quit): ")
-            var input = readLine()
-            if (input == "ls") {
-                println(s"${readMe.getParentFolder}")
-            }
-            if (input == "end") {
-                println("Ending task")
-                System.exit(0)
-                }
-            }
-        }
+// Content - files and folders
+@main
+def run(): Unit =
+    val fileSystemContent = Folder("home", List (
+        Folder("documents", List(
+            File("cv.pdf"),
+            File("data.dat")
+        )),
+        Folder("downloads", List(
+            Folder("msinstaller", List(
+                File("instmsia.exe")
+            ))
+        )),
+        Folder("music", List(
+            File("GOLDEN.mp3")
+        ))
+        Folder("photos", List(
+            File("passport.jpg"),
+            File("photoid.png"),
+            Folder("japan2026", List(
+                File("tokyo.png"),
+                File("kyoto.png"),
+                File("miyajima.png")
+            ))
+        )),
+        File("autoexec.bat"),
+        File("config.sys"),
+        File("command.com")
+    ))
+    val zipper = Zipper(fileSystem, Nil)
+    shell(zipper)
