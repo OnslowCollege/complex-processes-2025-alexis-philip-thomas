568299c0f42ba6d218ce47394d3cd21f3c8a899e
filip <philip.thomson@student.onslow.school.nz>
2025-10-30 21:37:21 +1300

    went through and commented all my code finally

--- src/main/scala/GUI.scala
+++ src/main/scala/GUI.scala
@@ -1,11 +1,13 @@
+// impoartant imports from files
 import javafx.application.Application
 import javafx.stage.Stage
 import javafx.scene.Scene
 import javafx.scene.control._
 import javafx.scene.layout._
 import javafx.geometry.Insets
-
 import scala.util.{Try, Success, Failure}
+
+// back end connection
 import filesystemBackend._
 
 object Main {
@@ -15,8 +17,12 @@ object Main {
 }
 
 class MyApp extends Application {
+  // mutable variable for an immutable file system gui
+  // starting as an empty zipper
   private var state: Zipper = _
+
   override def start(mainStage: Stage): Unit = {
+    // file system structure that contain files from the backend.
     val fileSystemGui = Folder("home", List (
       Folder("myfolder", List(
           File("read.txt"))),
@@ -24,18 +30,23 @@ class MyApp extends Application {
           File("testfile.exe"))),
     ))
     state = Zipper(fileSystemGui, Nil)
-
+    
+    // all ui components for the visuals
     val pathLabel = Label(printPath(state))
     val listViewing = ListView[String]()
     val inputArea = TextField()
     val outputArea = TextArea()
     outputArea.setEditable(false)
 
+    // creating a list view for thec current folder your in
     def reload(): Unit =
       pathLabel.setText(printPath(state))
       listViewing.getItems.clear()
       state.focus match
         case Folder(_, children) =>
+          // when the node if focused on is a folder - prints out its children and relevant information
+          
+          // seperating children into folders and files by alphebtical order.
           val folders: List[Folder] = children.collect { case d: Folder => d}.sortBy(_.name)
           val files: List[File] = children.collect { case f: File => f}.sortBy(_.name)
 
@@ -45,20 +56,27 @@ class MyApp extends Application {
             listViewing.getItems.add(s"$paddedOutput"))
         case File(_, _) =>
           None
+        
+        // clearing user input filed and changing the windows title to match the folder path.
         inputArea.clear
         mainStage.setTitle(printPath(state))
 
     reload()
 
+    // taking commands from the users input area
     inputArea.setOnAction(_ => { 
+      // spliting the users input different parts (command, target and modification) by " ".
       val input: String = inputArea.getText.toLowerCase.trim
       val parts: Array[String] = input.split(" ")
       
       if parts.size >= 1 then
         parts(0) match
           case "ls" =>
+            // "refreshes" the focused nodes children.
             outputArea.appendText("refreshed.\n")
           case "cd" =>
+            // changes the focused node to being the target. continues the shell with the new focus.
+            // if that is possible.
             if parts.size == 2 then
               val target: String = parts(1)
               if target == ".." then
@@ -74,12 +92,14 @@ class MyApp extends Application {
                   case None =>
                     outputArea.appendText("the system cannot find the path specified")
           case "mkdir" =>
+            // redefining the focus that adds the users folder in its list of children.
             if parts.size == 2 then
               val target: String = parts(1)
               state = mkDir(target, state)
             else
               outputArea.appendText("'mkdir' epects 1 parameter after command: mkdir <FOLDERNAME>")
           case "touch" =>
+            // dedefining the focus that adds the users file in its list of children.
             if parts.size == 2 then
               val target: String = parts(1)
               state = touch(target, minFileSize, state)
@@ -94,20 +114,30 @@ class MyApp extends Application {
             else
               outputArea.appendText("'touch' expects a max of two parameters: touch <FILENAME> <FILESIZE>\n")
           case "rm" =>
+            // redefining the focus to exclude the target file from its children.
             if parts.size == 2 then
               val target: String = parts(1)
               state = rm(target, state)
             else
               outputArea.appendText("'rm' expects 1 parameter: rm <FILENAME>\n")
           case "rmdir" =>
+            // redefining the focus to exclude the target folder from its children.
             if parts.size == 2 then
               val target: String = parts(1)
               state = rmdir(target, state)
             else
               outputArea.appendText("'rmdir' expects 1 parameter: rmdir <FOLDERNAME>\n")
+          case "taskkill" =>
+            // Quit the GUI program by closing the window.
+            mainStage.close
+          
+          case _ =>
+            // user inputs anything not included above (a.k.a a command)
+            outputArea.appendText(s"'${input}' is not recognised as a command please try again")
         reload()
     })
 
+    // layout for the gui's visuals
     val vbox = VBox(
       10,
       pathLabel,
